fc049913cdd2332b5b16873cf81a421ef467c2ff
Vsevolod Kompantsev <diditforlulz@gmail.com>
2023-01-22 20:25:11 +0300

    typing, naming, code style fixes

--- fastauc/cpp_auc.cpp
+++ fastauc/cpp_auc.cpp
@@ -8,23 +8,23 @@
 // Fill the zipped vector with pairs consisting of the
 // corresponding elements of a and b. (This assumes 
 // that the vectors have equal length)
-template<typename tuple_type> void zip(
+template <typename tuple_type> void zip(
     const bool* a, 
     const float* b,
     const float* sample_weight,
     const size_t len, 
     std::vector<tuple_type> &zipped)
-{
-    for(size_t i=0; i<len; ++i)
     {
-        if constexpr(std::is_same<tuple_type, std::tuple<bool, float, float>>::value){
-            zipped.push_back(std::make_tuple(a[i], b[i], sample_weight[i]));
-        }
-        else{
-            zipped.push_back(std::make_tuple(a[i], b[i]));
+        for(size_t i=0; i<len; ++i)
+        {
+            if constexpr(std::is_same<tuple_type, std::tuple<bool, float, float>>::value) {
+                zipped.push_back(std::make_tuple(a[i], b[i], sample_weight[i]));
+            }
+            else {
+                zipped.push_back(std::make_tuple(a[i], b[i]));
+            }
         }
     }
-}
 
 double trapezoid_area(double x1, double x2, double y1, double y2) {
   double dx = x2 - x1;
@@ -32,7 +32,7 @@ double trapezoid_area(double x1, double x2, double y1, double y2) {
   return dx * y1 + dy * dx / 2.0;
 }
 
-template<typename tuple_type> float auc_kernel(float* ts, bool* st, size_t len, float* sample_weight) {
+template <typename tuple_type> float auc_kernel(float* ts, bool* st, size_t len, float* sample_weight) {
   // sort the data
   // Zip the vectors together
   std::vector<tuple_type> zipped;
@@ -50,17 +50,17 @@ template<typename tuple_type> float auc_kernel(float* ts, bool* st, size_t len, 
   double tps = 0;
   double last_counted_fps = 0;
   double last_counted_tps = 0;
-  double auc = 0.0;
-  for(size_t i=0; i<zipped.size(); ++i) {
-    if constexpr(std::is_same<tuple_type, std::tuple<bool, float, float>>::value){
+  double auc = 0;
+  for (size_t i=0; i < zipped.size(); ++i) {
+    if constexpr(std::is_same<tuple_type, std::tuple<bool, float, float>>::value) {
         tps += std::get<0>(zipped[i]) * std::get<2>(zipped[i]);
         fps += (1 - std::get<0>(zipped[i])) * std::get<2>(zipped[i]);
     }
-    else{
+    else {
         tps += std::get<0>(zipped[i]);
         fps += (1 - std::get<0>(zipped[i]));
     }
-    if( (i == zipped.size() - 1) || (std::get<1>(zipped[i+1]) != std::get<1>(zipped[i])) ) {
+    if ((i == zipped.size() - 1) || (std::get<1>(zipped[i+1]) != std::get<1>(zipped[i]))) {
         auc += trapezoid_area(last_counted_fps, fps, last_counted_tps, tps);
         last_counted_fps = fps;
         last_counted_tps = tps;
@@ -71,10 +71,10 @@ template<typename tuple_type> float auc_kernel(float* ts, bool* st, size_t len, 
 
 extern "C" {
     float cpp_auc_ext(float* ts, bool* st, size_t len, float* sample_weight, size_t n_sample_weights) {
-        if(n_sample_weights > 0){
+        if(n_sample_weights > 0) {
             return auc_kernel<std::tuple<bool, float, float>>(ts, st, len, sample_weight);
         }
-        else{
+        else {
             return auc_kernel<std::tuple<bool, float>>(ts, st, len, sample_weight);
         }
     }
--- fastauc/fast_auc.py
+++ fastauc/fast_auc.py
@@ -21,13 +21,13 @@ class CppAuc:
                                              ]
         self._handle.cpp_auc_ext.restype = ctypes.c_float
 
-    def roc_auc_score(self, y_true: np.array, y_score: np.array, sample_weight=None) -> float:
+    def roc_auc_score(self, y_true: np.array, y_score: np.array, sample_weight: np.array=None) -> float:
         """a method to calculate AUC via C++ lib.
 
         Args:
             y_true (np.array): 1D numpy array of dtype=np.bool8 as true labels.
             y_score (np.array): 1D numpy array of dtype=np.float32 as probability predictions.
-            sample_weight (np.array): 1D numpy array as sample weights, default=empty array.
+            sample_weight (np.array): 1D numpy array as sample weights, optional.
 
         Returns:
             float: AUC score
@@ -42,25 +42,37 @@ class CppAuc:
         raise NotImplemented
         return np.array(result)
 
+def fast_numba_auc(y_true: np.array, y_score: np.array, sample_weight: np.array=None) -> float:
+    """a function to calculate AUC via python + numba.
+
+    Args:
+        y_true (np.array): 1D numpy array as true labels.
+        y_score (np.array): 1D numpy array as probability predictions.
+        sample_weight (np.array): 1D numpy array as sample weights, optional.
+
+    Returns:
+        AUC score as float
+    """
+    if sample_weight is None:
+        return fast_numba_auc_nonw(y_true=y_true, y_score=y_score)
+    else:
+        return fast_numba_auc_w(y_true=y_true, y_score=y_score, sample_weight=sample_weight)
+
+
 @numba.njit
-def trapezoid_area(x1, x2, y1, y2):
+def trapezoid_area(x1: float, x2: float, y1: float, y2: float) -> float:
     dx = x2 - x1
     dy = y2 - y1
     return dx * y1 + dy * dx / 2.0
 
-def fast_numba_auc(y_true: np.array, y_prob: np.array, sample_weight=None):
-    return fast_numba_auc_kernel(y_true,y_prob,sample_weight if sample_weight is not None else np.array([]))
-
 
 @numba.njit
-def fast_numba_auc_kernel(y_true: np.array, y_prob: np.array, sample_weight: np.array):
+def fast_numba_auc_nonw(y_true: np.array, y_score: np.array) -> float:
     y_true = (y_true == 1)
 
-    desc_score_indices = np.argsort(y_prob, kind="mergesort")[::-1]
-    y_score = y_prob[desc_score_indices]
+    desc_score_indices = np.argsort(y_score, kind="mergesort")[::-1]
+    y_score = y_score[desc_score_indices]
     y_true = y_true[desc_score_indices]
-    if len(sample_weight) > 0:
-        sample_weight = sample_weight[desc_score_indices]
 
     prev_fps = 0
     prev_tps = 0
@@ -68,9 +80,8 @@ def fast_numba_auc_kernel(y_true: np.array, y_prob: np.array, sample_weight: np.
     last_counted_tps = 0
     auc = 0.0
     for i in range(len(y_true)):
-        weight = (sample_weight[i] if len(sample_weight) > 0 else 1.0)
-        tps = prev_tps + y_true[i] * weight
-        fps = prev_fps + (1 - y_true[i]) * weight
+        tps = prev_tps + y_true[i]
+        fps = prev_fps + (1 - y_true[i])
         if i == len(y_true) - 1 or y_score[i+1] != y_score[i]:
             auc += trapezoid_area(last_counted_fps, fps, last_counted_tps, tps)
             last_counted_fps = fps
@@ -79,14 +90,40 @@ def fast_numba_auc_kernel(y_true: np.array, y_prob: np.array, sample_weight: np.
         prev_fps = fps
     return auc / (prev_tps*prev_fps)
 
+@numba.njit
+def fast_numba_auc_w(y_true: np.array, y_score: np.array, sample_weight: np.array) -> float:
+    y_true = (y_true == 1)
 
-def fast_auc(y_true: np.array, y_prob: np.array, sample_weight=None) -> Union[float, str]:
+    desc_score_indices = np.argsort(y_score, kind="mergesort")[::-1]
+    y_score = y_score[desc_score_indices]
+    y_true = y_true[desc_score_indices]
+    sample_weight = sample_weight[desc_score_indices]
+
+    prev_fps = 0
+    prev_tps = 0
+    last_counted_fps = 0
+    last_counted_tps = 0
+    auc = 0.0
+    for i in range(len(y_true)):
+        weight = sample_weight[i]
+        tps = prev_tps + y_true[i] * weight
+        fps = prev_fps + (1 - y_true[i]) * weight
+        if i == len(y_true) - 1 or y_score[i+1] != y_score[i]:
+            auc += trapezoid_area(last_counted_fps, fps, last_counted_tps, tps)
+            last_counted_fps = fps
+            last_counted_tps = tps
+        prev_tps = tps
+        prev_fps = fps
+    return auc / (prev_tps * prev_fps)
+
+
+def fast_auc(y_true: np.array, y_score: np.array, sample_weight: np.array=None) -> Union[float, str]:
     """a function to calculate AUC via python.
 
     Args:
         y_true (np.array): 1D numpy array as true labels.
         y_score (np.array): 1D numpy array as probability predictions.
-        sample_weight (np.array): 1D numpy array as sample weights, default=empty array.
+        sample_weight (np.array): 1D numpy array as sample weights, optional.
 
     Returns:
         float or str: AUC score or 'error' if imposiible to calculate
@@ -94,8 +131,8 @@ def fast_auc(y_true: np.array, y_prob: np.array, sample_weight=None) -> Union[fl
     # binary clf curve
     y_true = (y_true == 1)
 
-    desc_score_indices = np.argsort(y_prob, kind="mergesort")[::-1]
-    y_score = y_prob[desc_score_indices]
+    desc_score_indices = np.argsort(y_score, kind="mergesort")[::-1]
+    y_score = y_score[desc_score_indices]
     y_true = y_true[desc_score_indices]
     if sample_weight is not None:
         sample_weight = sample_weight[desc_score_indices]
